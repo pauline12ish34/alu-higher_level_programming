c1dd73f2a65fd367c94261808562dbc766ffc123
pauline12ish34 <paulineishimwe12@gmail.com>
2024-02-23 16:12:31 -0800

    checksign and last digit

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number > 0 :
+    print("the number is positive")
+else :
+    print("the number is negative")
---
+++ python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+If else condittions in python
