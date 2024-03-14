80a033752f06ac91c423cda51ca0205dd20c92d5
pauline12ish34 <paulineishimwe12@gmail.com>
2024-03-14 01:31:19 -0700

    validation of size

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,4 +6,8 @@ class Square:
     """Represent a square."""
     def __init__(self,size):
         """constructor for class properties"""
+        if not isinstance(size,int)
+            raise TyperError( "size should be an integer")
+        elif size < 0:
+            raise ValuerError("the size should be greater than zero")
         self.__size = size
