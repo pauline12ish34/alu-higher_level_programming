d08b9fe0e3e8b32511bc0c7c0771873b8725b4cd
pauline12ish34 <paulineishimwe12@gmail.com>
2024-03-22 03:50:04 -0700

    score name not null 16

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- no link 
+SELECT name,score FROM second_table WHERE name IS NOT NULL ORDER BY(score) DESC;
--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- record with id 89
-SELECT * FROM first_table WHERE id=89;
+SELECT COUNT(*) FROM first_table WHERE id=89;
